a1f23f2efc6068a8ca30dc6c8704f987fe5804f0
nadinewahdan <nadine.a.wahdan@gmail.com>
2015-12-15 12:02:47 +0000

    added controllers

--- db/migrate/20151215084656_create_follows.rb
+++ db/migrate/20151215084656_create_follows.rb
@@ -3,7 +3,7 @@ class CreateFollows < ActiveRecord::Migration
     create_table :follows do |t|
       t.references :follower, foreign_key: true, null: false, index: true
       t.references :followed, foreign_key: true, null: false, index: true
-      
+    
       t.timestamps null: false
     end
   end
