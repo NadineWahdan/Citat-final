788e888881426669f9c4e2d99d456d8840578fe3
omnyaamr <omnyaamro@gmail.com>
2015-12-15 12:29:58 +0000

    users_controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController <  Api::BaseController
+before_action:authenicate_post!, only: [ :show]
         def all_posts
             @posts = Post.all
             respond_with @posts
         end
+        
+        def show
+            @post= Post.find(params[:id])
+        end
+
+
+
+      def remove
+            Post.find(params[:id]).destroy
+            redirect_to :action => 'post.all'
+      end
+
+
+     def create
+            @posts = Post.new(post_params)
+     end
+        
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,27 @@
 class UsersController <  Api::BaseController
+before_action :authenticate_user! , only: [:show,:follower]
+
         def all_users
             @users = User.all
             respond_with @users
         end
+        
+        def new
+        @user = User.new
+        end
+
+    def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to root_path, notice: 'Registered! Please login'
+    else
+      render action: :new
+    end
+    end
+
+  def user_params
+    params.require(:user).permit(:facebook, :password)
+  end
+  
 end
