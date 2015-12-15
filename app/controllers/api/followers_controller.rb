d1b5618749c769e727170c8fba47de9f089a11fa
omnyaamr <omnyaamro@gmail.com>
2015-12-15 12:53:15 +0000

    followers_controller

--- app/controllers/api/followers_controller.rb
+++ app/controllers/api/followers_controller.rb
@@ -1,6 +1,21 @@
 class FollowersController <  Api::BaseController
+    
     def all_followers
             @followers = Followers.all
             respond_with @followers
-        end
+    end
+    
+    
+    def unfollow
+     Follow.find(params[:id]).destroy
+      redirect_to :action => 'all_followers'
+    end 
+    
+    def follow
+       @follow = follow.new(follow_params)
+        
+    end
+    
+    
+   
 end
